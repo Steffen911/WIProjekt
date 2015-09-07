ee9b50ea4e92024a510c297bf54a114610e6a20a
Steffen911 <steffenschmitz@hotmail.de>
2015-09-07 16:24:07 +0200

    TestDatei
    Eine Probedatei abgelegt.

---
+++ login.php
@@ -0,0 +1,50 @@
+<?php session_start();
+if (!empty($_POST['email']) AND !empty($_POST['pwd']))
+{
+	
+//DB Verbindung
+require_once 'settings.php';
+	
+//Felder übergeben
+$Email = $_POST['email'];
+$Password = sha1($_POST['pwd']);	
+
+//Abfrage in der DB
+$Query = mysql_query("SELECT * FROM User WHERE Email='$Email' AND Passwort='$Password'");
+$NumRows = mysql_num_rows($Query);
+
+if($NumRows != 0)
+{
+	while($Row = mysql_fetch_assoc($Query))
+	{
+		$Id = $Row['Id'];
+		
+		if($Row['ReservationCat']!=0){
+			$_SESSION['ReservationCat'] = $Row['ReservationCat'];
+		}
+	}
+}
+else
+{
+	die("Email oder Passwort falsch!");
+	exit();
+}
+
+//Sessiondaten schreiben
+$_SESSION['name'] = $Email;
+$_SESSION['uId'] = $Id;
+
+//Link Weiterleitung
+$cat = $_POST['cat'];
+$activemenu = $_POST['activemenu'];
+
+if(isset($_POST['cat']) AND isset($_POST['activemenu'])){	header('Location: index.php?cat='.$cat.'&activemenu='.$activemenu.'');}
+else{														header('Location: index.php');}  
+
+exit();
+}
+else
+{
+    echo "Login fehlgeschlagen!";
+}
+?>
