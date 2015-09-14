c6ce7163576cb5ea8c59b94182bb8700bc875ba1
Steffen911 <steffenschmitz@hotmail.de>
2015-09-14 16:24:47 +0200

    Schnittstelle FileReader erstellt
    returns a string array
    0 freigabe
    1 satzstatus
    2 gegnerzug
    3 sieger

--- rocket/src/schnittstellen/main.java
+++ rocket/src/schnittstellen/main.java
@@ -1,15 +1,21 @@
 package schnittstellen;
 
-import java.io.IOException;
-
 public class main {
 
-	public static void main(String[] args) throws IOException {
+	public static void main (String[] args) {
 		
 
-		schnittstellen sst = new schnittstellen("X", "C:\\test\\");
-		sst.fileWriter(1);
+		schnittstellen sst = new schnittstellen("x", "//Users//Steffen//Dropbox//WI-Projekt//Server+Technik//");
+/*
+		try {
+			sst.fileWriter(1);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+*/
 		
+		sst.fileReader();
 	}
-
+	
 }
--- rocket/src/schnittstellen/schnittstellen.java
+++ rocket/src/schnittstellen/schnittstellen.java
@@ -1,16 +1,20 @@
 package schnittstellen;
 
 import java.io.*;
+import javax.xml.parsers.*;
+
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
 
 public class schnittstellen {
 	
 	private String spielerwahl;
 	
-	//Muss folgendes Muster erfüllen: C:\\test\\
+	//Muss folgendes Muster erfuellen: C:\\test\\
 	private String dateipfad;
 	
-	//Übergabewert (spielerwahl) gibt an, welcher Spieler zugewiesen wurde
-	//Übergabewert (dateipfad) gibt an, welcher der Kommunikationspfad ist
+	//Uebergabewert (spielerwahl) gibt an, welcher Spieler zugewiesen wurde
+	//Uebergabewert (dateipfad) gibt an, welcher der Kommunikationspfad ist
 	public schnittstellen (String spielerwahl, String dateipfad)
 	{
 		this.spielerwahl = spielerwahl;
@@ -18,33 +22,85 @@ public class schnittstellen {
 	}
 	
 	//File-Schnittstelle: AgentFile erstellen
-	//Übergabewert (spielzug) gibt an, in welche Spalte gelegt werden soll
+	//Uebergabewert (spielzug) gibt an, in welche Spalte gelegt werden soll
 	public void fileWriter (int spielzug) throws IOException {
 	
-		//Auswahl, ob Spieler X oder O gewählt wurde
-		//"X" = Spieler X
-		//"O" = Spieler O
+		//Auswahl, ob Spieler x oder o gewaehlt wurde
+		//"x" = Spieler x
+		//"o" = Spieler o
 		
-		//Switch Case für jeweilige Erstellung des AgentFiles
+		//Switch Case fuer jeweilige Erstellung des AgentFiles
 		switch (spielerwahl) {
-		case "X":
+		case "x":
 			//Dateischreibobjekt deklarieren und erzeugen
 			FileWriter fileWriterX = new FileWriter(dateipfad + "spielerx2server.txt");
 			// Spaltenwahl in eine Datei schreiben
 			fileWriterX.write(Integer.toString(spielzug));
-			//Stream schließen
+			//Stream schliessen
 			fileWriterX.close();
 			break;
-		case "O":
+		case "o":
 			//Dateischreibobjekt deklarieren und erzeugen
 			FileWriter fileWriterO = new FileWriter("C:\\eclipse\\spielero2server.txt");
 			// Spaltenwahl in eine Datei schreiben
 			fileWriterO.write(Integer.toString(spielzug));
-			//Stream schließen
+			//Stream schliessen
 			fileWriterO.close();
 			break;
 		default:
 				System.out.println("Spielerwahl falsch!");
 		}
-}
+	}
+	
+	//File-Schnittstelle: ServerFile lesen
+	public String[] fileReader () {
+		
+		//String[0] = freigabe
+		//String[1] = satzstatus
+		//String[2] = gegnerzug
+		//String[3] = sieger
+		String returnString[] = new String[4];
+		
+		DocumentBuilderFactory builderFactory =
+		        DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = null;
+		try {
+		    builder = builderFactory.newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
+		    e.printStackTrace();  
+		}
+		
+		try {
+		    Document document = builder.parse(
+		        new FileInputStream(dateipfad + "server2spieler" + spielerwahl + ".xml"));
+		    
+		    Element element = document.getDocumentElement();
+		    
+		    NodeList nodes = element.getChildNodes();
+
+		    int j = 0;
+		    for(int i=0; i<nodes.getLength(); i++){
+		      Node node = nodes.item(i);
+
+		      if(node instanceof Element){
+		        //a child element to process
+		        Element child = (Element) node;
+		        
+		        returnString[j] = child.getTextContent();
+		        j++;
+		      }
+
+		    }
+		    
+		} catch (SAXException e) {
+		    e.printStackTrace();
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
+		
+		for (int i=0; i<returnString.length; i++){
+			System.out.println(returnString[i]);
+		}
+		return returnString;
+	}
 }
