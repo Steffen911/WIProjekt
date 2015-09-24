ee190d0eb8d56b1d245d104504052c19cb32c2cb
Steffen911 <steffenschmitz@hotmail.de>
2015-09-24 11:29:45 +0200

    timer korrigiert

--- rocketFX/src/serverKommunikation/TimerStart.java
+++ rocketFX/src/serverKommunikation/TimerStart.java
@@ -7,10 +7,8 @@ public class TimerStart{
 	public void startTimer(int sekunden){
 		Timer timer = new Timer();
 
-		// Start in 2 Sekunden
+		// Start in int sekunden
     	timer.schedule( new Task(), (sekunden * 1000) );
 
-		// Start in einer Sekunde dann Ablauf alle 5 Sekunden
-	    //timer.schedule( new Task(), 1000, 5000 );
 	}
 }
